c023b4d252706089d7eeb1db6b571adcabfff8ac
arnor-nolen <andrei.gorbulin@rightware.com>
2023-05-02 17:21:51 +0300

    Applied buffer_scale to cursor

--- dwlb.c
+++ dwlb.c
@@ -584,9 +584,10 @@ pointer_enter(void *data, struct wl_pointer *pointer,
 	}
 
 	if (!cursor_image) {
-		struct wl_cursor_theme *cursor_theme = wl_cursor_theme_load(NULL, 24, shm);
+		struct wl_cursor_theme *cursor_theme = wl_cursor_theme_load(NULL, 24 * buffer_scale, shm);
 		cursor_image = wl_cursor_theme_get_cursor(cursor_theme, "left_ptr")->images[0];
 		cursor_surface = wl_compositor_create_surface(compositor);
+        wl_surface_set_buffer_scale(cursor_surface, buffer_scale);
 		wl_surface_attach(cursor_surface, wl_cursor_image_get_buffer(cursor_image), 0, 0);
 		wl_surface_commit(cursor_surface);
 	}
